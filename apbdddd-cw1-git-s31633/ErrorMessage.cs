6854e2ba87cf9dc50e2fad27958a71c6f896bb24
balko <balkonvdome@gmail.com>
2026-03-16 20:08:21 +0100

    Added Error Message

---
+++ apbdddd-cw1-git-s31633/ErrorMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apbdddd_cw1_git_s31633
+{
+    internal class ErrorMessage
+    {
+    }
+}
