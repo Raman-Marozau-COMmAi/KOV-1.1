c6835896b7ad680e6f16f004b0f9f6b854d87825
balko <s31633@pjwstk.edu.pl>
2026-03-16 20:25:26 +0100

    Added "data request"

---
+++ apbdddd-cw1-git-s31633/DataRequest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace apbdddd_cw1_git_s31633
+{
+    internal class DataRequest
+    {
+
+        String data;
+
+        public DataRequest() {
+
+            Console.WriteLine("Enter your data:");
+            data = Console.ReadLine();
+            
+
+        }
+
+        public void PrintData() {
+
+
+            Console.WriteLine("Your data is: " + data);
+        
+        
+        }
+
+    }
+}
